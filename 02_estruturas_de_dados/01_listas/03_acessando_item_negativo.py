ec71b768a74a7a1aa3dcf781fccf18ae29c87f42
Elvis Maicol <elvism.estudo@gmail.com>
2024-04-24 00:08:59 -0300

    V2 - Primeira etapa

---
+++ 02_estruturas_de_dados/01_listas/03_acessando_item_negativo.py
@@ -0,0 +1,6 @@
+
+# Acessando item usando valor negativo
+frutas = ["maçã", "laranja", "uva", "pera"]
+
+print(frutas[-1])  # pera
+print(frutas[-3])  # laranja
---
+++ 02_estruturas_de_dados/01_listas/07_compreensao_listas.py
@@ -0,0 +1,17 @@
+#  Compreensão de listas
+
+# Filtrar lista
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = []
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
+
+pares = [numero for numero in numeros if numero % 2 == 0]
+
+print(pares)
+
+# Modificar valores
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = [numero**2 for numero in numeros]
+print(quadrado)
