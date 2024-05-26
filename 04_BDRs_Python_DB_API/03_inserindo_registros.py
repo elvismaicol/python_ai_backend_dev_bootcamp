48926b50272ae4b7014025e17d1a4a2605c88c2c
Elvis Maicol <elvism.estudo@gmail.com>
2024-05-26 20:11:35 -0300

    V4 - Inserindo dados

---
+++ 04_BDRs_Python_DB_API/03_inserindo_registros.py
@@ -0,0 +1,16 @@
+import sqlite3
+from pathlib import Path
+
+
+ROOT_PATH = Path(__file__).parent
+
+conexao = sqlite3.connect(ROOT_PATH / "meu_banco.db")
+cursor = conexao.cursor()
+
+#cursor.execute(
+#    "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))"
+#)
+
+data = ("Juliana", "ju@xxx.com")
+cursor.execute("INSERT INTO clientes (nome, email) VALUES (?,?);", data)
+conexao.commit()
